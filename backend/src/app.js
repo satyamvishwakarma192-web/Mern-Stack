c4dc5fcddae462cce64eac64a89b79823e750ee6
Satyam33-cv <242451903+satyamvishwakarma192-web@users.noreply.github.com>
2026-07-10 00:13:46 +0530

    for saving token in cookie,jwt,login and register controllers
    importing jwt and cookie-parser package,cookie-parser at middleware && jwt at controllers & exporting login and register controllers by creating object in module.exports

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 // create server 
 const express = require('express');
-
+// cookie-parser 
+const cookieParser = require("cookie-parser");//  for saving tokens of user
 const app = express();
+
+
+
+app.use(express.json());// middle ware
+app.use(cookieParser())   // cookie parser is created in middleware
 app.get("/",(req,res) =>  //api created here <<---
 {
     res.send("Hello world")
--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,9 +1,11 @@
  
  //logic -- controller is created for callback req and res 
-const bcrypt
- //
- //iport usermodel
- const userModel = require("../models/users.model")
+const bcrypt = requuire("bcryptjs");
+ //import bcrypt
+const jwt = require("jsonwebtoken"); 
+// import jwt
+//import usermodel
+ const userModel = require("../models/users.model");
 
 async function registerUser(req,res){  // async functions
     const { FullName,Email,password} = req.body;   // get user data from req.body
@@ -13,9 +15,38 @@ async function registerUser(req,res){  // async functions
     // ML of middleware in >> ./app.js
 
     const isUserAlreadyExists = await userModel.findOne({Email}) // for checking that used email is already exit or not 
+    
     if(isUserAlreadyExists){ // if it exist then status will be 400 !! 
         return res.status(400).json({
             message:" User already exists"
         })
     }
+    const hashedPassword = await bcrypt.hash(password,10); // password hashing thru bcrypt
+    const user = await userModel.create({
+        FullName,Email,password: hashedPassword
+    })// finally userr registerd
+    const token = jwt.sign({
+        id:user._id,},"de08efc2e1b2b31077bdc64e43ea4a2161adc4cba869b3449bc94fab3c814dec")
+    
+    
+    res.cookie("token",token)
+    res.status(201).json({
+        message:"user  registered successfully",
+        user:{
+        _id: user._id,
+        Email: user.Email,
+        FullName: user.FullName
+        
+
+        }
+        
+     })
+
 }
+
+async function loginUser(req,res){}//login user 
+
+module.exports = {
+    registerUser,
+    loginUser
+} // by creating Object exporting bhot user controllers
