addbdbcba1cde878a52cf64430c09a2c1185c774
Satyam33-cv <242451903+satyamvishwakarma192-web@users.noreply.github.com>
2026-07-09 21:20:21 +0530

    some comment for db and server.js file
    basic steps with the help of comments

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 // server start 
 const app = require('./src/app');
-const connectDB = require("./src/db/db.js"); // importing the db 
+const connectDB = require("./src/db/db.js"); // importing the db and function is called
 //connecting database from monogodb
 
 connectDB();
+//call
+
 app.listen(3000,() =>{
 
 console.log("server is running on the port 3000")
 });
+// server will start by nodemon server.js CMD and then MonogoDb is Connected successfully...
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
+// logic -- how to connect server to our DB
 
-
-
-function connectDB() {
-    mongoose.connect("mongodb://atlas-sql-6a4ea616d6dc363bc5cc1a6a-u48czw.a.query.mongodb.net/food-view?ssl=true&authSource=admin")
+// function for connecting to db
+function connectDB() { 
+    mongoose.connect("mongodb://atlas-sql-6a4ea616d6dc363bc5cc1a6a-u48czw.a.query.mongodb.net/food-view?ssl=true&authSource=admin") //url of cluster1 for connection to db cluster1.m7t9ojd.mongodb.net
         .then(() => {
-            console.log("MongoDB connected");
+            console.log("MongoDB connected"); //callback when our db is connected successfully
         })
         .catch((err) => {
-            console.log("MongoDB connection error:", err);
-        })
+            console.log("MongoDB connection error:", err); 
+        })// by default any error or cause happen err console 
 }
 
-module.exports = connectDB;
+module.exports = connectDB; // exporting db 
