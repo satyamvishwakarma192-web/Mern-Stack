cd5d368258ec27097ddefbd127f46e2ee4fc445c
Satyam33-cv <242451903+satyamvishwakarma192-web@users.noreply.github.com>
2026-07-09 21:52:55 +0530

    added func for checking email is already exists or not and password hashed
    using (isuseralreadyexists) and hashing password by bcryptjs npm package

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
  
  //logic -- controller is created for callback req and res 
-
+const bcrypt
  //
  //iport usermodel
  const userModel = require("../models/users.model")
@@ -11,4 +11,11 @@ async function registerUser(req,res){  // async functions
     // but the data will be not getting from ,cause when express default server is created bydef that server cant read data from req.body
     // & heres its solnn >--middle ware [ for get data and send to req.body and it make it readable for server]
     // ML of middleware in >> ./app.js
+
+    const isUserAlreadyExists = await userModel.findOne({Email}) // for checking that used email is already exit or not 
+    if(isUserAlreadyExists){ // if it exist then status will be 400 !! 
+        return res.status(400).json({
+            message:" User already exists"
+        })
+    }
 }
